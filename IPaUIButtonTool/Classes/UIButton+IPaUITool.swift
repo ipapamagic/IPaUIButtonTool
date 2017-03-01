bcb3072f3beac81ab6b43b41e5772631ed851dac
IPa Chen <ipapamagic@gmail.com>
2017-03-01 14:12:32 +0800

    Update content size when using center image up  and title down mode

--- IPaUIButtonTool/Classes/UIButton+IPaUITool.swift
+++ IPaUIButtonTool/Classes/UIButton+IPaUITool.swift
@@ -15,23 +15,32 @@ extension UIButton
             return
         }
         let imageSize = image.size
+        var x = -imageSize.width * 0.5
+        var y = (imageSize.height + space) * 0.5
         titleEdgeInsets = UIEdgeInsetsMake(
-            0.0, -imageSize.width, -(imageSize.height + space), 0.0)
+            y, x, -y, -x)
         
         // raise the image and push it right so it appears centered
         //  above the text
         let titleSize = titleText.size(attributes: [NSFontAttributeName: titleLabel.font])
+        x = titleSize.width * 0.5
+        y = -(titleSize.height + space) * 0.5
         imageEdgeInsets = UIEdgeInsetsMake(
-        -(titleSize.height + space), 0.0, 0.0, -titleSize.width)
+            y, x, -y, -x)
+        let width = max(imageSize.width,titleSize.width)
+        let height = imageSize.height + titleSize.height + space
+        x = (width - bounds.width) * 0.5
+        y = (height - bounds.height) * 0.5
+        contentEdgeInsets = UIEdgeInsetsMake(y, x, y, x)
     }
     open func imageAlignRight(_ space:CGFloat) {
         guard let imageView = imageView,let titleLabel = titleLabel else {
             return
         }
-        titleEdgeInsets = UIEdgeInsetsMake(0, -imageView.frame.size.width - space, 0, imageView.frame.size.width);
-        imageEdgeInsets = UIEdgeInsetsMake(0, titleLabel.frame.size.width + space, 0, -titleLabel.frame.size.width);
+        titleEdgeInsets = UIEdgeInsetsMake(0, -imageView.frame.size.width - space, 0, imageView.frame.size.width)
+        imageEdgeInsets = UIEdgeInsetsMake(0, titleLabel.frame.size.width + space, 0, -titleLabel.frame.size.width)
         
-        contentEdgeInsets = UIEdgeInsetsMake(0, space, 0, space);
+        contentEdgeInsets = UIEdgeInsetsMake(0, space, 0, space)
     }
 }
 
