d7204dd71299aa8ca35a70aaf7ab3fc64a9cbc33
IPa Chen <ipapamagic@gmail.com>
2019-10-16 04:03:52 +0800

    update to swift 5.0

--- IPaUIButtonTool/Classes/UIButton+IPaUITool.swift
+++ IPaUIButtonTool/Classes/UIButton+IPaUITool.swift
@@ -17,30 +17,30 @@ extension UIButton
         let imageSize = image.size
         var x = -imageSize.width * 0.5
         var y = (imageSize.height + space) * 0.5
-        titleEdgeInsets = UIEdgeInsetsMake(
-            y, x, -y, -x)
+        titleEdgeInsets = UIEdgeInsets(
+            top: y, left: x, bottom: -y, right: -x)
         
         // raise the image and push it right so it appears centered
         //  above the text
-        let titleSize = titleText.size(withAttributes: [NSAttributedStringKey.font: titleLabel.font])
+        let titleSize = titleText.size(withAttributes: [NSAttributedString.Key.font: titleLabel.font])
         x = titleSize.width * 0.5
         y = -(titleSize.height + space) * 0.5
-        imageEdgeInsets = UIEdgeInsetsMake(
-            y, x, -y, -x)
+        imageEdgeInsets = UIEdgeInsets(
+            top: y, left: x, bottom: -y, right: -x)
         let width = max(imageSize.width,titleSize.width)
         let height = imageSize.height + titleSize.height + space
         x = (width - bounds.width) * 0.5
         y = (height - bounds.height) * 0.5
-        contentEdgeInsets = UIEdgeInsetsMake(y, x, y, x)
+        contentEdgeInsets = UIEdgeInsets(top: y, left: x, bottom: y, right: x)
     }
     @objc open func imageAlignRight(_ space:CGFloat) {
         guard let imageView = imageView,let titleLabel = titleLabel else {
             return
         }
-        titleEdgeInsets = UIEdgeInsetsMake(0, -imageView.frame.size.width - space, 0, imageView.frame.size.width)
-        imageEdgeInsets = UIEdgeInsetsMake(0, titleLabel.frame.size.width + space, 0, -titleLabel.frame.size.width)
+        titleEdgeInsets = UIEdgeInsets(top: 0, left: -imageView.frame.size.width - space, bottom: 0, right: imageView.frame.size.width)
+        imageEdgeInsets = UIEdgeInsets(top: 0, left: titleLabel.frame.size.width + space, bottom: 0, right: -titleLabel.frame.size.width)
         
-        contentEdgeInsets = UIEdgeInsetsMake(0, space, 0, space)
+        contentEdgeInsets = UIEdgeInsets(top: 0, left: space, bottom: 0, right: space)
     }
 }
 
